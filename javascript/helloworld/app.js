50b4858706185eac28387e8c4622593f0c783eb8
mazharaheer <mazharhameed17@gmail.com>
2017-07-13 22:24:28 +0500

    helloworld

---
+++ javascript/helloworld/app.js
@@ -0,0 +1,10 @@
+// alert("Mazhar");
+var firstName = "Mazhar ";  // string variable
+var lastName = " Aheer";
+var age = 18; //number type variable
+
+var fullName;
+
+fullName = firstName + lastName;  // + is concetination. + is used to join
+
+alert("My Name is " + firstName + lastName + " " + "\n My age is " + age); //    \n for new line
